168dbd09f56be8cde5ab5af9524456162471d484
wangjiaxiang <wangjiaxiang@8haodi.com>
2019-03-14 18:48:16 +0800

    d

--- src/main/java/com/wjx/sjsr/controller/LoginController.java
+++ src/main/java/com/wjx/sjsr/controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
-@CrossOrigin("*")
+@CrossOrigin(origins = "*",allowCredentials="true")
 public class LoginController {
 
 
